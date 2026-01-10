ba5014effaa01d892e8226180b147f6ed0fdf62b
Surya HA <suryahariharan2006@gmail.com>
2026-01-10 19:35:50 +0530

    Updated code base

---
+++ frontend/vite.config.js
@@ -0,0 +1,26 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/compare': 'http://localhost:8000',
+      '/health': 'http://localhost:8000'
+    }
+  },
+  build: {
+    outDir: 'build',
+    sourcemap: false,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          utils: ['axios']
+        }
+      }
+    }
+  }
+})
